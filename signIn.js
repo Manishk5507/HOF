1f74d7c7b1a962b9feccc8a957ffc0213b525f94
Manish <7055ranamanish@gmail.com>
2024-04-07 04:38:40 +0530

    configuring routes

--- signIn.js
+++ signIn.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
     const password = document.getElementById('password').value;
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://hof-r7pv.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
